9ea9c9c85fb9391d5c51084845515ef66ee89834
KishorPokharel <kishorpokharel7@gmail.com>
2023-12-07 16:45:36 +0545

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/KishorPokharel/casa
 
 go 1.21.0
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/gabriel-vasile/mimetype v1.4.3
+	github.com/google/uuid v1.4.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
+
+require golang.org/x/net v0.17.0 // indirect
--- handlers.go
+++ handlers.go
@@ -1,36 +1,81 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gabriel-vasile/mimetype"
+	"github.com/google/uuid"
 )
 
-func handleHomePage(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./ui/templates/layout.html",
-		"./ui/templates/partials/header.html",
-		"./ui/templates/pages/home.html",
-	}
-	tmpl := template.Must(template.ParseFiles(files...))
-	err := tmpl.ExecuteTemplate(w, "layout", "Hello World")
+func (app *application) handleHomePage(w http.ResponseWriter, r *http.Request) {
+	page := "./ui/templates/pages/home.html"
+	err := app.render(w, page, nil)
 	if err != nil {
-		log.Println(err)
+		app.logger.Error(err.Error())
 	}
 }
 
-func handleNewProperty(w http.ResponseWriter, r *http.Request) {
+func (app *application) handleRegisterPage(w http.ResponseWriter, r *http.Request) {
+	page := "./ui/templates/pages/register.html"
+	err := app.render(w, page, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+	}
 }
 
-func handleNewPropertyPage(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./ui/templates/layout.html",
-		"./ui/templates/partials/header.html",
-		"./ui/templates/pages/property_create.html",
-	}
-	tmpl := template.Must(template.ParseFiles(files...))
-	err := tmpl.ExecuteTemplate(w, "layout", nil)
+func (app *application) handleLoginPage(w http.ResponseWriter, r *http.Request) {
+	page := "./ui/templates/pages/login.html"
+	err := app.render(w, page, nil)
 	if err != nil {
-		log.Println(err)
+		app.logger.Error(err.Error())
+	}
+}
+
+const imageDir = "./"
+
+func (app *application) handleNewProperty(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		app.logger.Error("could not parse form", "msg", err.Error())
+		http.Error(w, "could not parse form", http.StatusBadRequest)
+		return
+	}
+	uploadedFile, _, err := r.FormFile("thumbnail")
+	if err != nil {
+		app.logger.Error("r.FormFile", "msg", err.Error())
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	defer uploadedFile.Close()
+
+	mtype, err := mimetype.DetectReader(uploadedFile)
+	fmt.Println("Extension: ", mtype.Extension())
+	return
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	name := uuid.NewString()
+	out, err := os.Create(filepath.Join(imageDir, "banner_"+name))
+	defer out.Close()
+	_, err = io.Copy(out, uploadedFile)
+	if err != nil {
+		app.logger.Error("copy image to uploads", "msg", err.Error())
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, "Uploaded file")
+}
+
+func (app *application) handleNewPropertyPage(w http.ResponseWriter, r *http.Request) {
+	page := "./ui/templates/pages/property_create.html"
+	err := app.render(w, page, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
 	}
 }
