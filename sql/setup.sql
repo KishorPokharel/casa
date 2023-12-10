649a11f47b7aec7ac9031f152f8995c5e8b581aa
KishorPokharel <kishorpokharel7@gmail.com>
2023-12-10 10:33:42 +0545

    update sql table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,22 @@
-/* create table users if not exists ( */
-/*     username text not null, */
-/*     email citext not null, */
-/*     password bytea not null, */
-/*     phone text, */
-/*     user_type {buyer, seller} */
-/* ); */
+create extension if not exists citext;
 
-create table if not exists property_listings (
+create table if not exists users (
+    id bigserial primary key,
+    created_at timestamp(0) with time zone not null default now(),
+    username text not null,
+    email citext unique not null,
+    password_hash bytea not null,
+    phone text,
+    version integer not null default 1
+);
+
+create table if not exists listings (
+    id bigserial primary key,
+    created_at timestamp(0) with time zone not null default now(),
     description text not null,
     banner text not null,
     pictures text[],
     location text not null,
-    /* location_point geography not null, */
     property_type text not null check (property_type in ('apartment', 'land', 'house')),
     price int not null
 );
