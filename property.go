e1963aac49e0e4df3eaea2945365043924bb2bd8
KishorPokharel <kishorpokharel7@gmail.com>
2023-12-15 22:09:47 +0545

    handle listing and creating property

--- property.go
+++ property.go
@@ -7,7 +7,11 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strconv"
+	"strings"
 
+	"github.com/KishorPokharel/casa/storage"
+	"github.com/KishorPokharel/casa/validator"
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/google/uuid"
 )
@@ -15,20 +19,41 @@ import (
 func (app *application) handleHomePage(w http.ResponseWriter, r *http.Request) {
 	page := "./ui/templates/pages/home.html"
 	data := app.newTemplateData(r)
+	listings, err := app.storage.Property.GetAll()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	data.Listings = listings
 	app.render(w, r, http.StatusOK, page, data)
 }
 
 const imageDir = "./uploads/"
 
+type propertyCreateForm struct {
+	// ListingType   string
+	// PropertyType  string
+	Title string
+	// Province      string
+	Location string
+	Price    int64
+	ImageURL string
+	// FurnishStatus string
+	Description string
+	validator.Validator
+}
+
 func (app *application) handleNewProperty(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseMultipartForm(10 << 20)
 	if err != nil {
+		app.logger.Error(err.Error())
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 	uploadedFile, _, err := r.FormFile("thumbnail")
 	if err != nil {
-		app.serverError(w, r, err)
+		app.logger.Error(err.Error())
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 	defer uploadedFile.Close()
@@ -39,7 +64,9 @@ func (app *application) handleNewProperty(w http.ResponseWriter, r *http.Request
 		return
 	}
 	mtype := mimetype.Detect(b)
+
 	allowedExtensions := []string{".jpg", ".jpeg", ".png"}
+
 	ext := mtype.Extension()
 	if !slices.Contains(allowedExtensions, ext) {
 		app.logger.Warn("invalid image")
@@ -47,6 +74,40 @@ func (app *application) handleNewProperty(w http.ResponseWriter, r *http.Request
 		return
 	}
 	name := fmt.Sprintf("banner_%s%s", uuid.NewString(), ext)
+
+	form := propertyCreateForm{
+		Title:       r.FormValue("title"),
+		Location:    r.FormValue("location"),
+		ImageURL:    name,
+		Description: r.FormValue("description"),
+	}
+	priceString := r.FormValue("price")
+
+	// validate form
+	form.CheckField(validator.NotBlank(priceString), "price", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Location), "location", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Description), "description", "This field can not be blank")
+
+	if strings.TrimSpace(priceString) != "" {
+		price, err := strconv.Atoi(priceString)
+		if err != nil {
+			app.logger.Error(err.Error())
+			app.clientError(w, http.StatusBadRequest)
+			return
+		}
+		form.Price = int64(price)
+		form.CheckField(form.Price > 0, "price", "Price must greater than 0")
+	}
+
+	page := "./ui/templates/pages/property_create.html"
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, page, data)
+		return
+	}
+
 	out, err := os.Create(filepath.Join(imageDir, name))
 	defer out.Close()
 	_, err = out.Write(b)
@@ -54,12 +115,26 @@ func (app *application) handleNewProperty(w http.ResponseWriter, r *http.Request
 		app.serverError(w, r, err)
 		return
 	}
-	fmt.Fprintln(w, "Uploaded file")
+	property := storage.Property{
+		Banner:      form.ImageURL,
+		Location:    form.Location,
+		Title:       form.Title,
+		Description: form.Description,
+		Price:       form.Price,
+	}
+	err = app.storage.Property.Insert(property)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	app.sessionManager.Put(r.Context(), sessionFlashKey, "Created a property")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *application) handleNewPropertyPage(w http.ResponseWriter, r *http.Request) {
 	page := "./ui/templates/pages/property_create.html"
 	data := app.newTemplateData(r)
+	data.Form = propertyCreateForm{}
 	app.render(w, r, http.StatusOK, page, data)
 }
 
