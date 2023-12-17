0656037e0281dd7aef0e9e061d3792453be01be9
KishorPokharel <kishorpokharel7@gmail.com>
2023-12-17 08:17:27 +0545

    read id from params

--- helpers.go
+++ helpers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -30,4 +34,14 @@ func (app *application) notFound(w http.ResponseWriter, r *http.Request) {
 	page := "./ui/templates/pages/notfound.html"
 	data := app.newTemplateData(r)
 	app.render(w, r, http.StatusNotFound, page, data)
+}
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
 }
