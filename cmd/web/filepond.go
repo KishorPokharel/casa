9a2b53c3db993dc0e57badbf23afe772dc8b5bea
KishorPokharel <kishorpokharel7@gmail.com>
2024-02-22 00:14:57 +0545

    can add more pictures

--- cmd/web/filepond.go
+++ cmd/web/filepond.go
@@ -7,7 +7,11 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strconv"
+	"strings"
 
+	"github.com/KishorPokharel/casa/storage"
+	"github.com/KishorPokharel/casa/validator"
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/google/uuid"
 )
@@ -56,5 +60,99 @@ func (app *application) handleFileUpload(name string) func(w http.ResponseWriter
 	}
 }
 
+type propertyCreateForm2 struct {
+	Title       string
+	Location    string
+	Price       int64
+	Thumbnail   string
+	Pictures    []string
+	Description string
+	validator.Validator
+}
+
 func (app *application) handleNewListingFilepond(w http.ResponseWriter, r *http.Request) {
+	userID := app.sessionManager.GetInt64(r.Context(), sessionAuthKey)
+
+	err := r.ParseMultipartForm(5 << 20) // 5MB
+	if err != nil {
+		app.logger.Error(err.Error())
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := propertyCreateForm2{
+		Title:       r.FormValue("title"),
+		Location:    r.FormValue("location"),
+		Thumbnail:   r.FormValue("thumbnail"),
+		Description: r.FormValue("description"),
+		Pictures:    r.Form["picture"],
+	}
+	priceString := r.FormValue("price")
+
+	// validate form
+	form.CheckField(validator.NotBlank(priceString), "price", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Location), "location", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Description), "description", "This field can not be blank")
+	form.CheckField(validator.NotBlank(form.Thumbnail), "thumbnail", "This field can not be blank")
+
+	// TODO: validate pictures array
+
+	if strings.TrimSpace(priceString) != "" {
+		price, err := strconv.Atoi(priceString)
+		if err != nil {
+			app.logger.Error(err.Error())
+			app.clientError(w, http.StatusBadRequest)
+			return
+		}
+		form.Price = int64(price)
+		form.CheckField(form.Price > 0, "price", "Price must greater than 0")
+	}
+
+	page := "./ui/templates/pages/property_create_filepond.html"
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, page, data)
+		return
+	}
+
+	// move files from tmp to uploads directory
+
+	// move thumbnail image
+	oldImagePath := filepath.Clean(filepath.Join("./tmp/", form.Thumbnail))
+	newImagePath := filepath.Clean(filepath.Join("./uploads/", form.Thumbnail))
+	if err := os.Rename(oldImagePath, newImagePath); err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	// move additional pictures
+	for _, picture := range form.Pictures {
+		oldImagePath := filepath.Clean(filepath.Join("./tmp/", picture))
+		newImagePath := filepath.Clean(filepath.Join("./uploads/", picture))
+		if err := os.Rename(oldImagePath, newImagePath); err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+	}
+
+	property := storage.Property2{
+		Banner:      form.Thumbnail,
+		Location:    form.Location,
+		Title:       form.Title,
+		Description: form.Description,
+		Price:       form.Price,
+		UserID:      userID,
+		Pictures:    form.Pictures,
+	}
+
+	err = app.storage.Property.Insert2(property)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), sessionFlashKey, "Created a property")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
