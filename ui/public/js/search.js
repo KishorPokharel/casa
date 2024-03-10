da3952c4a97e76566963fa1545353c0fd8684525
KishorPokharel <kishorpokharel7@gmail.com>
2024-03-10 11:20:18 +0545

    location fuzzy search on frontend

--- ui/public/js/search.js
+++ ui/public/js/search.js
@@ -1,25 +1,45 @@
 const searchInput = document.querySelector('#searchInput');
+const locationResults = document.querySelector('#locationResults');
 
-searchInput.addEventListener('keyup', showSuggestions);
-
-var locations;
-var fuse;
-
-fetch('/locations')
-  .then(res => res.json())
-  .then(data => {
-    // locations = data.locations;
-    fuse = new Fuse(data.locations, {includeScore: true});
-    console.log(data);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+searchInput.addEventListener('keyup', debounce(showSuggestions), 800);
 
 function showSuggestions(e) {
-  const value = e.target.value;
-  console.log('Locations: ', locations);
-  const results = fuse.search(value);
-  console.log(results);
+  const value = e.target.value.trim();
+  if (value === '') {
+    return;
+  }
+  const queryString = new URLSearchParams({query: value}).toString();
+  fetch(`/locations?${queryString}`)
+    .then(res => res.json())
+    .then(data => {
+      let html = "";
+      for (let match of data.matches) {
+        html += `<li class="location" data-text="${match.Target}">${match.Target}</li>`;
+      }
+      locationResults.innerHTML = html;
+
+      // listen for click on new elements
+      const locationBoxes = document.querySelectorAll('.location');
+      locationBoxes.forEach(box => {
+        box.addEventListener("click", (e) => {
+          const target = e.target;
+          searchInput.value = target.dataset.text;
+          locationResults.innerHTML = "";
+          searchInput.focus();
+        });
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }
 
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
