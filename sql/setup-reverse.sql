25a3e28d0af728e6c0c035d9e6a5c4cdec33fcbd
KishorPokharel <kishorpokharel7@gmail.com>
2024-03-13 21:54:52 +0545

    update

--- sql/setup-reverse.sql
+++ sql/setup-reverse.sql
@@ -1,4 +1,8 @@
 drop table if exists favorites;
+drop table if exists pictures;
+drop table if exists messages;
+drop table if exists users_rooms;
 drop table if exists listings;
 drop table if exists users;
 drop table if exists sessions;
+drop table if exists rooms;
